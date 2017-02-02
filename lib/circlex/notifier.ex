c76998ab37f092fbf6a5b0b984541579d998d82e
Bruno Buccolo <bruno.buccolo@gmail.com>
2017-02-02 09:30:46 -0200

    Adds title to notifications

--- lib/circlex/notifier.ex
+++ lib/circlex/notifier.ex
@@ -1,18 +1,22 @@
 defmodule Circlex.Notifier do
   def status_change(repo, branch, "", initial_status) do
-    notify("Build is " <> initial_status)
+    notify(title(repo, branch), "Build is " <> initial_status)
   end
 
   def status_change(repo, branch, previous_status, status) do
-    notify("Build is now " <> status <> ", previously it was " <> previous_status)
+    notify(title(repo, branch), "Build is now " <> status <> ", previously it was " <> previous_status)
   end
 
   def idle() do
     IO.puts('.')
   end
 
-  defp notify(message) do
-    display_command = "display notification \"" <> message <> ".\""
+  defp title(repo, branch) do
+    repo <> " @ " <> branch
+  end
+
+  defp notify(title, message) do
+    display_command = "display notification \"" <> message <> ".\" with title \"" <> title <> ".\""
     System.cmd("osascript", ["-e", String.to_charlist(display_command)], [stderr_to_stdout: true])
   end
 end
