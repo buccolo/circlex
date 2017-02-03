fd58a3b609c352628d89be02af1ff4aa32fe7d6b
Bruno Buccolo <bruno.buccolo@gmail.com>
2017-02-02 22:40:20 -0200

    Handldes project 404 on watcher

--- lib/circlex/watcher.ex
+++ lib/circlex/watcher.ex
@@ -1,15 +1,19 @@
 defmodule Circlex.Watcher do
   def watch(repo, branch, previous_status \\ "") do
-    {:ok, status } = Circlex.Checker.check(repo, branch)
+    case Circlex.Checker.check(repo, branch) do
+      {:ok, status } ->
 
-    if previous_status != status do
-      Circlex.Notifier.status_change(repo, branch, previous_status, status)
-    else
-      Circlex.Notifier.idle()
+        if previous_status != status do
+          Circlex.Notifier.status_change(repo, branch, previous_status, status)
+        else
+          Circlex.Notifier.idle()
+        end
+
+        :timer.sleep(5000)
+        watch(repo, branch, status)
+      {_, message } ->
+        IO.puts(message)
     end
-
-    :timer.sleep(5000)
-    watch(repo, branch, status)
   end
 end
 
